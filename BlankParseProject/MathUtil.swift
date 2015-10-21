f9c5314c48d59755e0996ba9e2a7af9d5964a297
Xinxing Jiang <jiangxinxing09@gmail.com>
2015-10-21 10:10:50 -0700

    Implement a stupid random number generator.

---
+++ BlankParseProject/MathUtil.swift
@@ -0,0 +1,27 @@
+//
+//  MathUtil.swift
+//  11bnb.demo
+//
+//  Created by Xinxing Jiang on 10/15/15.
+//  Copyright © 2015 iosjjj. All rights reserved.
+//
+
+import Foundation
+
+class MathUtil {
+    class func indicator(x: Double) -> Int {
+        return (x >= 0 ? 1 : -1)
+    }
+    
+    class func indicator(x: Bool) -> Int {
+        return (x ? 1 : -1)
+    }
+    
+    // generate a random integer in range [low, high]
+    class func randomNumber(low low: Int, high: Int) -> Int? {
+        if low < high {
+            return Int(arc4random()) % (high - low + 1) + low
+        }
+        return nil
+    }
+}
--- BlankParseProject/ViewController.swift
+++ BlankParseProject/ViewController.swift
@@ -9,17 +9,84 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Subviews
+    
+    var label: UILabel!
+    
+    // MARK: - VC life cycles
+    
+    override func loadView() {
+        super.loadView()
+        self.view.backgroundColor = Constants.BackgtoundColor
+        initLabel()
+    }
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad()        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    // MARK: - Init label
+    
+    private func initLabel() {
+        label = UILabel()
+        label.text = Constants.LabelTextStatic
+        label.textColor = Constants.LabelTextColor
+        self.view.addSubview(label)
+        
+        // constraints
+        label.translatesAutoresizingMaskIntoConstraints = false
+        let centerXCon = NSLayoutConstraint(item: label, attribute: .CenterX, relatedBy: .Equal, toItem: self.view, attribute: .CenterX, multiplier: 1, constant: 0)
+        let centerYCon = NSLayoutConstraint(item: label, attribute: .CenterY, relatedBy: .Equal, toItem: self.view, attribute: .CenterY, multiplier: 1, constant: 0)
+        self.view.addConstraints([centerXCon, centerYCon])
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func canBecomeFirstResponder() -> Bool {
+        return true
     }
-
-
+    
+    // MARK: - Shake motion recognizer
+    
+    override func motionBegan(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        if motion == .MotionShake {
+            label.text = Constants.LabelTextDynamic
+        }
+    }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        if motion == .MotionShake {
+            // generate a random numebr
+            if let randomNumber = MathUtil.randomNumber(low:Constants.LowerBound, high: Constants.HighBound) {
+                label.text = "\(Constants.LabelTextReslt) \(randomNumber)"
+            }
+        }
+    }
+    
+    // MARK: - Constants
+    
+    struct Constants {
+        static let BackgtoundColor = UIColor.blackColor()
+        
+        static let LabelTextStatic = "Shake your phone!"
+        static let LabelTextColor = UIColor.whiteColor()
+        static let LabelTextDynamic = "I'm shaking!"
+        static let LabelTextReslt = "Here you go:"
+        
+        static let LowerBound = 1
+        static let HighBound = 10
+    }
 }
 
